a1f563b118b079d0f7ea440e0e489e5b1bfc72bc
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-05-12 02:38:04 +0530

    Add project files.

---
+++ encoding-module-win11sdk.cpp
@@ -0,0 +1,12 @@
+﻿// encoding-module-win11sdk.cpp : Defines the entry point for the application.
+//
+
+#include "encoding-module-win11sdk.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ encoding-module-win11sdk.h
@@ -0,0 +1,8 @@
+﻿// encoding-module-win11sdk.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
